be90fe8eb65f8a96c6de6bacae5273fca5f1b9a2
Ho Thi Khanh Ly <khanhlypnv17@gmail.com>
2024-02-26 20:06:47 +0700

    add props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,13 @@
-
-import Button from './Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
-
-  return (
-    <>
-      <Button />
-    </>
-  );
+	return (
+		<>
+			<Student name="Ly" age={30} isStudent={true} />
+			<Student name="Keo Em" age={42} isStudent={false} />
+			<Student name="CHange" />
+		</>
+	);
 }
 
-export default App
+export default App;
